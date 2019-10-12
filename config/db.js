936f8afdcdd5099dd12bcef8c036b67083eba7ce
Abhivarsh <abhivarsh.pnv@gmail.com>
2019-10-12 16:14:13 -0400

    Express and MongoDB Setup

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected successfully!!');
+  } catch (err) {
+    console.error(err.message);
+    //Exit the process with a failure.
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app
-  .get('/', (req, res) => res.send('API Runnning'))
-  .get('/welcome', (req, res) => res.send('welcome to the site'));
+//connect to DB
+connectDB();
 
+app.get('/', (req, res) => res.send('API Runnning'));
+
+//Define routes
+app
+  .use('/api/users', require('./routes/api/users'))
+  .use('/api/posts', require('./routes/api/posts'))
+  .use('/api/profile', require('./routes/api/profile'))
+  .use('/api/auth', require('./routes/api/auth'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
